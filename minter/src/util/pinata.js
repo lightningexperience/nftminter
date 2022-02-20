82a73208f1d448941f7a305b564f4863d4c8a4fe
lightningexperience <lightningexperience@users.noreply.github.com>
2022-02-20 11:58:43 -0500

    Update pinata.js

--- minter/src/util/pinata.js
+++ minter/src/util/pinata.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const key = process.env.REACT_APP_PINATA_KEY;
-const secret = process.env.REACT_APP_PINATA_SECRET;
+const key = process.env.34854ccf05158987ba66;
+const secret = process.env.51b16e519995e24a4d52737b390851b29cb37c82c35a232cc9ae3b94ad5c7cde;
 const axios = require('axios');
 
 export const pinJSONToIPFS = async(JSONBody) => {
